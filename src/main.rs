d8166e887899954645c1979baaa37cbd024ba452
DeltaManiac <maxpaynered@gmail.com>
2019-01-20 20:45:38 +0530

    Hello World

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let x = 200;
+    let y = 100;
+    println!("P3\n{} {}\n255", x, y);
+    for j in (0..=y).rev() {
+        for i in 0..x {
+            let r = i as f64 / x as f64;
+            let g = j as f64 / y as f64;
+            let b = 0.2;
+            let ir = (255.99 * r) as u16;
+            let ig = (255.99 * g) as u16;
+            let ib = (255.99 * b) as u16;
+            println!("{} {} {}", ir, ig, ib)
+        }
+    }
 }
